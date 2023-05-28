815d0d2568d8cc31d31246f7c2408acb779d0bcb
dorapika <dorapika@gmail.com>
2023-05-28 14:44:31 +0800

    update many servants

--- dataSource.js
+++ dataSource.js
@@ -20,7 +20,18 @@ var AlterEgo = [
     steps: 206396,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/10015000.png"}
 }];
-var Foreigner = [];
+var Foreigner = [
+{
+    name: "北齋人特攻",
+    steps: 149860,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/25002000.png"}
+}, {
+    name: "北齋",
+    steps: 99965,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/25002000.png"}
+}
+
+    ];
 var Pretender = [];
 var Berserker = [
 {
@@ -37,7 +48,16 @@ var MoonCancer = [
     name: "水院三層特攻",
     steps: 283997,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/23004000.png"}
-}, ];
+}, {
+    name: "象神秩序特攻",
+    steps: 235893,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/23003000.png"}
+},  {
+    name: "象神",
+    steps: 157320,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/23003000.png"}
+}, 
+];
 var Avenger = [
 {
     name: "仇凜",
@@ -55,7 +75,12 @@ var Avenger = [
     name: "仇尼托",
     steps: 156663,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/11015000.png"}
-}];
+}, {
+    name: "蘭丸",
+    steps: 205023,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/11012000.png"}
+}
+];
 var Ruler = [
 {
     name: "瓊安雙特攻",
@@ -96,7 +121,12 @@ var Saber = [
     name: "村正",
     steps: 179464,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/1049000.png"}
-},];
+}, {
+    name: "兩儀式",
+    steps: 190978,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/1028000.png"}
+},
+];
 
 var Archer = [
 {
@@ -110,7 +140,16 @@ var Lancer = [
     name: "龍馬",
     steps: 197288,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/3050000.png"}
-},];
+}, {
+    name: "福利多神性特攻",
+    steps: 255076,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/3046000.png"}
+}, {
+    name: "福利多",
+    steps: 196212,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/3046000.png"}
+}
+];
 
 var Rider = [
 {
@@ -133,6 +172,14 @@ var Rider = [
     name: "曲亭馬琴",
     steps: 136501,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4048000.png"}
+},{
+    name: "小文西",
+    steps: 137582,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4035000.png"}
+}, {
+    name: "哈貝喵",
+    steps: 192148,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4042000.png"}
 },];
 
 var Caster = [
@@ -140,19 +187,61 @@ var Caster = [
     name: "大文西",
     steps: 223741,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5009000.png"}
-},];
+}, {
+    name: "不夜術王特攻",
+    steps: 285324,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5030000.png"}
+}, {
+    name: "不夜術",
+    steps: 142662,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5030000.png"}
+}, {
+    name: "紫式部魔性特攻",
+    steps: 246450,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5042000.png"}
+},{
+    name: "紫式部",
+    steps: 126385,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5042000.png"}
+}, {
+    name: "皇女",
+    steps: 119013,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5035000.png"}
+}, {
+    name: "齊格龍特攻",
+    steps: 308853,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5038000.png"}
+},{
+    name: "齊格",
+    steps: 154426,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5038000.png"}
+},
+];
 
 var Assassin = [
 {
     name: "煙霧鏡雙特攻",
     steps: 460451,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6047000.png"}
-},{
+}, {
     name: "煙霧鏡神性/超巨大特攻",
     steps: 307017,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6047000.png"}
 }, {
+    name: "酒吞",
+    steps: 242226,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6021000.png"}
+}, {
     name: "煙霧鏡",
     steps: 204728,
     pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6047000.png"}
-},];
+}, {
+    name: "水女武神地特攻",
+    steps: 235191,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6043000.png"}
+}, {
+    name: "水女武神",
+    steps: 156860,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6043000.png"}
+},
+];
