e010495d328ac147d69b8b2718a817e3eda24f19
dorapika <dorapika@gmail.com>
2023-06-22 14:07:02 +0800

    fix bug of duplicate classes

---
+++ dataSource2.js
@@ -0,0 +1,273 @@
+var AllElements = [
+{
+    name: "太歲人特攻",
+    steps: 479663,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/10012000.png"},
+    features: ["AlterEgo","人"]
+}, {
+    name: "神父惡特攻",
+    steps: 309594,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/10015000.png"},
+    features: ["AlterEgo","惡"]
+}, {
+    name: "太歲",
+    steps: 290782,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/10012000.png"},
+    features: ["AlterEgo","無"]
+}, {
+    name: "院院",
+    steps: 265370,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/10003000.png"},
+    features: ["AlterEgo","無"]
+}, {
+    name: "神父",
+    steps: 206396,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/10015000.png"},
+    features: ["AlterEgo","無"]
+}, {
+    name: "北齋人特攻",
+    steps: 149860,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/25002000.png"},
+    features: ["Foreigner","人"]
+}, {
+    name: "北齋",
+    steps: 99965,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/25002000.png"},
+    features: ["Foreigner","無"]
+}, {
+    name: "伊吹地特攻",
+    steps: 372640,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/7043000.png"},
+    features: ["Berserker","地"]
+},  {
+    name: "伊吹",
+    steps: 225941,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/7043000.png"},
+    features: ["Berserker","無"]
+} , {
+    name: "水院三層特攻",
+    steps: 283997,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/23004000.png"},
+    features: ["MoonCancer","精神"]
+}, {
+    name: "象神秩序特攻",
+    steps: 235893,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/23003000.png"},
+    features: ["MoonCancer","秩序"]
+},  {
+    name: "象神",
+    steps: 157320,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/23003000.png"},
+    features: ["MoonCancer","無"]
+}, {
+    name: "仇凜",
+    steps: 253659,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/11009000.png"},
+    features: ["Avenger","無"]
+}, {
+    name: "水迦摩魅惑特攻",
+    steps: 244763,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/11011000.png"},
+    features: ["Avenger","魅惑"]
+}, {
+    name: "仇尼托人特攻",
+    steps: 211408,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/11015000.png"},
+    features: ["Avenger","人"]
+}, {
+    name: "仇尼托",
+    steps: 156663,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/11015000.png"},
+    features: ["Avenger","無"]
+}, {
+    name: "蘭丸",
+    steps: 205023,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/11012000.png"},
+    features: ["Avenger","無"]
+}, {
+    name: "瓊安雙特攻",
+    steps: 306246,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/9016000.png"},
+    features: ["Ruler","人","惡"]
+}, {
+    name: "壹與神性特攻",
+    steps: 379702,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/9015000.png"},
+    features: ["Ruler","神性"]
+}, {
+    name: "壹與",
+    steps: 253135,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/9015000.png"},
+    features: ["Ruler","無"]
+}, {
+    name: "小教授善特攻",
+    steps: 234286,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/9013000.png"},
+    features: ["Ruler","善"]
+},  {
+    name: "瓊安人特攻",
+    steps: 204164,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/9016000.png"},
+    features: ["Ruler","人"]
+},  {
+    name: "瓊安惡特攻",
+    steps: 185603,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/9016000.png"},
+    features: ["Ruler","惡"]
+}, {
+    name: "小教授",
+    steps: 156190,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/9013000.png"},
+    features: ["Ruler","無"]
+}, {
+    name: "瓊安",
+    steps: 123735,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/9016000.png"},
+    features: ["Ruler","無"]
+}, {
+    name: "村正王特攻",
+    steps: 215357,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/1049000.png"},
+    features: ["Saber","王"]
+}, {
+    name: "村正",
+    steps: 179464,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/1049000.png"},
+    features: ["Saber","無"]
+}, {
+    name: "兩儀式",
+    steps: 190978,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/1028000.png"},
+    features: ["Saber","無"]
+}, {
+    name: "泊貞",
+    steps: 132133,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/2032000.png"},
+    features: ["Archer","無"]
+}, {
+    name: "龍馬",
+    steps: 197288,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/3050000.png"},
+    features: ["Lancer","無"]
+}, {
+    name: "福利多神性特攻",
+    steps: 255076,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/3046000.png"},
+    features: ["Lancer","神性"]
+}, {
+    name: "福利多",
+    steps: 196212,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/3046000.png"},
+    features: ["Lancer","無"]
+}, {
+    name: "曲亭馬琴雙特攻",
+    steps: 266035,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4048000.png"},
+    features: ["Rider","惡","魔性"]
+}, {
+    name: "曲亭馬琴惡特攻",
+    steps: 204677,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4048000.png"},
+    features: ["Rider","惡"]
+}, {
+    name: "曲亭馬琴魔性特攻",
+    steps: 177407,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4048000.png"},
+    features: ["Rider","魔性"]
+}, {
+    name: "奧德修斯",
+    steps: 146513,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4038000.png"},
+    features: ["Rider","無"]
+}, {
+    name: "曲亭馬琴",
+    steps: 136501,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4048000.png"},
+    features: ["Rider","無"]
+},{
+    name: "小文西",
+    steps: 137582,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4035000.png"},
+    features: ["Rider","無"]
+}, {
+    name: "哈貝喵",
+    steps: 192148,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/4042000.png"},
+    features: ["Rider","無"]
+}, {
+    name: "大文西",
+    steps: 223741,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5009000.png"},
+    features: ["Caster","無"]
+}, {
+    name: "不夜術王特攻",
+    steps: 285324,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5030000.png"},
+    features: ["Caster","王"]
+}, {
+    name: "不夜術",
+    steps: 142662,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5030000.png"},
+    features: ["Caster","無"]
+}, {
+    name: "紫式部魔性特攻",
+    steps: 246450,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5042000.png"},
+    features: ["Caster","魔性"]
+},{
+    name: "紫式部",
+    steps: 126385,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5042000.png"},
+    features: ["Caster","無"]
+}, {
+    name: "皇女",
+    steps: 119013,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5035000.png"},
+    features: ["Caster","無"]
+}, {
+    name: "齊格龍特攻",
+    steps: 308853,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5038000.png"},
+    features: ["Caster","龍"]
+},{
+    name: "齊格",
+    steps: 154426,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/5038000.png"},
+    features: ["Caster","無"]
+}, {
+    name: "煙霧鏡雙特攻",
+    steps: 460451,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6047000.png"},
+    features: ["Assassin","神性","超巨大"]
+}, {
+    name: "煙霧鏡神性特攻",
+    steps: 307017,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6047000.png"},
+    features: ["Assassin","神性"]
+}, {
+    name: "煙霧鏡超巨大特攻",
+    steps: 307017,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6047000.png"},
+    features: ["Assassin","超巨大"]
+}, {
+    name: "酒吞",
+    steps: 242226,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6021000.png"},
+    features: ["Assassin","無"]
+}, {
+    name: "煙霧鏡",
+    steps: 204728,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6047000.png"},
+    features: ["Assassin","無"]
+}, {
+    name: "水女武神地特攻",
+    steps: 235191,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6043000.png"},
+    features: ["Assassin","地"]
+}, {
+    name: "水女武神",
+    steps: 156860,
+    pictureSettings: {src: "https://kazemai.github.io/fgo-vz/common/images/icon/faces/6043000.png"},
+    features: ["Assassin","無"]
+},
+];
